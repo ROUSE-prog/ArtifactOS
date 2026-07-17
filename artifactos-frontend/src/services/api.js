e8629871201b405767f36f398fe2a7e724cefac0
ROUSE-prog <stevenr2049@gmail.com>
2026-07-17 08:49:10 -0400

    config: support production API URL

--- artifactos-frontend/src/services/api.js
+++ artifactos-frontend/src/services/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+const api = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:5000/api",
 });
+
+export default api;
